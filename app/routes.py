726e1374054b17bf727d9bafc241168c46c99aee
Umit Aslan <umitaslan@u.northwestern.edu>
2020-08-17 18:21:59 -0500

    reset code function implemented
    also added a simple html importer but it sucks right now

--- app/routes.py
+++ app/routes.py
@@ -19,12 +19,14 @@ def primitive(primitive_name):
     
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     md = os.path.join(BASE_DIR, 'static/md/' + pn +'.md')
-    nlcode = os.path.join(BASE_DIR, 'static/nlogo/' + pn +'.nlcode')
+    nlcode = os.path.join(BASE_DIR, 'static/nlcode/' + pn +'.nlcode')
+    mdl = os.path.join(BASE_DIR, 'static/nlweb/' + pn +'.html')
     
     if os.path.exists(md):
         with open(md, 'r') as d: 
             with open(nlcode, 'r') as nc:
-                return render_template('primitive.html', primitive = pn, description = d.read(), code = nc.read())
+                with open(mdl, 'r') as m:
+                    return render_template('primitive.html', primitive = pn, description = d.read(), code = nc.read(), model = m.read())
     else:
         ## MAKE THIS RENDER A 404 File!!!
         return 'I don\'t know what ' + pn + ' is :('
