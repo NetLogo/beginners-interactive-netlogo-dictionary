69e1b207709b1c2c18ee5fbc0e4a53ede494f015
Umit Aslan <umitaslan@u.northwestern.edu>
2020-08-22 16:36:15 -0500

    fixed a bunch of responsiveness issues + syntax highlighting in links

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
-#from flaskext.markdown import Markdown
 
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 fa = FontAwesome(app)
-#Markdown(app)
 
 from app import routes
