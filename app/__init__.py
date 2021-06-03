b31cdfb9a04c5985d341bea9ceacf271ca0b9b5e
Umit Aslan <umitaslan@u.northwestern.edu>
2021-06-03 12:18:58 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 
-
-
 app = Flask(__name__)
 fa = FontAwesome(app)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
